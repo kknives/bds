f10880e305342eb05ffe038395d1f53d96efcd15
Shashwat Ganesh <shashwatganesh@protonmail.com>
2024-10-08 12:21:37 +0530

    sort: implement selection sort

---
+++ src/sort.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+fn sort(array: []u32) void {
+    if (array.len < 2) return;
+    if (array.len == 2) sort2(array);
+}
+fn sort2(array: [2]u32) void {
+    if (array[0] < array[1]) return;
+    std.mem.swap(u32, array[0], array[1]);
+}
+fn insertion_sort(array: []u32) void {
+    _ = array;
+}
+fn selection_sort(array: []u32) void {
+    const n = array.len;
+    var i: usize = 0;
+    if (n == 0) return;
+    while (i < n - 1) : (i += 1) {
+        var j: usize = i + 1;
+        var min_idx = i;
+        while (j < n) : (j += 1) {
+            min_idx = if (@min(array[min_idx], array[j]) != array[min_idx]) j else min_idx;
+        }
+        std.mem.swap(u32, &array[min_idx], &array[i]);
+    }
+}
+fn quick_sort(array: []u32) void {
+    _ = array;
+}
+fn merge_sort(array: []u32) void {
+    _ = array;
+}
+
+test "sort trivial array" {
+    var trivial_array = [_]u32{};
+    selection_sort(&trivial_array);
+    try std.testing.expect(trivial_array.len == 0);
+}
+
+test "two element array" {
+    var array = [_]u32{ 2, 0 };
+    selection_sort(&array);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 2 }, &array);
+}
+
+test "three element array" {
+    var array = [_]u32{ 90, 32, 29 };
+    selection_sort(&array);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 29, 32, 90 }, &array);
+}
+
+test "five element array" {
+    var array = [_]u32{ 90, 32, 2, 19, 29 };
+    selection_sort(&array);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 2, 19, 29, 32, 90 }, &array);
+}
+
+test "duplicates in array" {
+    var array = [_]u32{ 1, 1, 2, 2, 2, 3, 1, 1, 0 };
+    selection_sort(&array);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 1, 1, 1, 1, 2, 2, 2, 3 }, &array);
+}
+// Can't really test stability using just U32 values
+// test "stability" {
+//     var array = [_]u32{ 1, 1, 2, 2, 2, 3, 1, 1};
+//     const first_one_ptr = &array[0];
+//     selection_sort(&array);
+//     try std.testing.expectEqual(array[first_one_ptr], )
+//     try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 1, 1, 1, 1, 2, 2, 2, 3 }, &array);
+// }
