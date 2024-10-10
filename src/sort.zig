953b1d150f2626aac79fe0b1c4abc70958fc467b
Shashwat Ganesh <shashwatganesh@protonmail.com>
2024-10-10 19:51:40 +0530

    sort: implement mergesort with in place merging

--- src/sort.zig
+++ src/sort.zig
@@ -48,11 +48,46 @@ fn shell_sort(array: []u32) void {
         }
     }
 }
+fn merge_sort(array: []u32, left: usize, right: usize) void {
+    if (left >= right) return;
+    const mid = (left + right) >> 1;
+    merge_sort(array, left, mid);
+    merge_sort(array, mid + 1, right);
+    in_place_merge(array, left, mid, right);
+}
 fn quick_sort(array: []u32) void {
     _ = array;
 }
-fn merge_sort(array: []u32) void {
-    _ = array;
+fn in_place_merge(array: []u32, left: usize, mid: usize, right: usize) void {
+    var i = left;
+    const j = mid + 1;
+    while (i < mid + 1) {
+        if (array[i] <= array[j]) {
+            i += 1;
+            continue;
+        }
+        const tmp = array[i];
+        array[i] = array[j];
+        // Binary search to insert tmp into array[j+1..]
+        const S = struct {
+            fn lower_u32(context: void, lhs: u32, rhs: u32) bool {
+                _ = context;
+                return lhs < rhs;
+            }
+        };
+        const insert_pos = std.sort.upperBound(u32, tmp, array[j + 1 .. right + 1], {}, S.lower_u32);
+        // if (insert_pos == array[j + 1 ..].len) {
+        //     std.debug.print("LowerBound reached end of array. Val: {}\n", .{tmp});
+        //     // insert_pos = array.len - 1;
+        // }
+        var k = j;
+        while (k < j + insert_pos) : (k += 1) {
+            array[k] = array[k + 1];
+        }
+        array[k] = tmp;
+        // Binary search done
+        i += 1;
+    }
 }
 
 test "sort trivial array" {
@@ -66,6 +101,10 @@ test "sort trivial array" {
 
     trivial_array = [_]u32{};
     shell_sort(&trivial_array);
+    try std.testing.expect(trivial_array.len == 0);
+
+    trivial_array = [_]u32{};
+    merge_sort(&trivial_array, 0, 0);
     try std.testing.expect(trivial_array.len == 0);
 }
 
@@ -81,6 +120,10 @@ test "two element array" {
     array = [_]u32{ 2, 0 };
     shell_sort(&array);
     try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 2 }, &array);
+
+    array = [_]u32{ 2, 0 };
+    merge_sort(&array, 0, 1);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 2 }, &array);
 }
 
 test "three element array" {
@@ -94,6 +137,10 @@ test "three element array" {
 
     array = [_]u32{ 90, 32, 29 };
     shell_sort(&array);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 29, 32, 90 }, &array);
+
+    array = [_]u32{ 90, 32, 29 };
+    merge_sort(&array, 0, 2);
     try std.testing.expectEqualSlices(u32, &[_]u32{ 29, 32, 90 }, &array);
 }
 
@@ -109,6 +156,10 @@ test "five element array" {
     array = [_]u32{ 90, 32, 2, 19, 29 };
     shell_sort(&array);
     try std.testing.expectEqualSlices(u32, &[_]u32{ 2, 19, 29, 32, 90 }, &array);
+
+    array = [_]u32{ 90, 32, 2, 19, 29 };
+    merge_sort(&array, 0, array.len - 1);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 2, 19, 29, 32, 90 }, &array);
 }
 
 test "duplicates in array" {
@@ -122,6 +173,10 @@ test "duplicates in array" {
 
     array = [_]u32{ 1, 1, 2, 2, 2, 3, 1, 1, 0 };
     shell_sort(&array);
+    try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 1, 1, 1, 1, 2, 2, 2, 3 }, &array);
+
+    array = [_]u32{ 1, 1, 2, 2, 2, 3, 1, 1, 0 };
+    merge_sort(&array, 0, array.len - 1);
     try std.testing.expectEqualSlices(u32, &[_]u32{ 0, 1, 1, 1, 1, 2, 2, 2, 3 }, &array);
 }
 // Can't really test stability using just U32 values
